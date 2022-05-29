78f8a084b1ba4dc06e956d57fe890086f7dd18ae
Frehiwet Tsegay <ftsegay@miu.edu>
2022-05-28 22:39:01 -0500

    added new files

---
+++ Javascript/practice.js
@@ -0,0 +1,8 @@
+window.onload = function(){
+
+let clickButton = document.getElementById("button");
+clickButton.onclick =clicked;
+
+let clickButton = document.getElementById("button");
+clickButton2.onclick =clicked;
+}
