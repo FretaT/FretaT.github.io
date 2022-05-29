16e0c83c969fd265277c4d2ae8dd9f0e7af880b0
FretaT <106114039+FretaT@users.noreply.github.com>
2022-05-29 03:13:26 -0500

    Delete Javascript directory

--- Javascript/practice.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-
-let clickButton = document.getElementById("button");
-clickButton.onclick =clicked;
-
-let clickButton = document.getElementById("button");
-clickButton2.onclick =clicked;
-}
